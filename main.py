d956caf46a132ef51aaff5a71cb3846d9509ce5f
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2019-09-11 00:08:26 +0300

    finished initial implementation and added a todo

--- main.py
+++ main.py
@@ -1,97 +1,182 @@
 import sys
 import re
-from enum import auto, Enum
+import translations
+from translations import *
 
-class OPCODES(Enum):
-    END = "0" 
-    NOP = auto()
-    PUSHI = auto()
-    PUSHL = auto()
-    PUSHR = auto()
-    POP = auto()
-    LDR = auto()
-    STR = auto()
-    MOVRI = auto() 
-    MOVRL = auto() 
-    MOVRR = auto()
-    ADDRI = auto() 
-    ADDRL = auto() 
-    ADDRR = auto()
-    SUBRI = auto() 
-    SUBRL = auto() 
-    SUBRR = auto()
-    MULRI = auto() 
-    MULRL = auto() 
-    MULRR = auto()
-    DIVRI = auto() 
-    DIVRL = auto() 
-    DIVRR = auto()
-    IMULRI  = auto()
-    IMULRL  = auto()
-    IMULRR = auto()
-    IDIVRI  = auto()
-    IDIVRL  = auto()
-    IDIVRR  = auto()
-    CMPRI  = auto()
-    CMPRL   = auto()
-    CMPRR = auto()
-    JMP  = auto()
-    JNZ  = auto()
-    JZ  = auto()
-    JG  = auto()
-    JGE  = auto()
-    JL  = auto()
-    JLE  = auto()
-    JA  = auto()
-    JAE  = auto()
-    JB  = auto()
-    JBE = auto()
-    NOT = auto()
-    ANDRI  = auto()
-    ANDRL  = auto()
-    ANDRR = auto()
-    ORRI  = auto()
-    ORRL  = auto()
-    ORRR = auto()
-    XORRI  = auto()
-    XORRL = auto()
-    XORRR = auto()
+"""
+TODO:
+1. Add support for labels
+2. Add support for the data segment
+"""
 
 def exit_error(msg):
     print(msg)
     sys.exit(msg)
 
-def translate_register(reg):
-    reg_dict = {
-    "R0": "0", 
-    "R1": "1", 
-    "R2": "2", 
-    "R3": "3", 
-    "R4": "4", 
-    "R5": "5", 
-    "R6": "6", 
-    "R7": "7",
-    "R8": "8",
-    "R9": "9",
-    "R10": "a",
-    "R11": "b",
-    "R12": "c",
-    "R13": "d",
-    "R14": "e",
-    "R15": "f",
-    "FL": "c",
-    "PC": "d",
-    "BP": "e",
-    "SP": "f"
-    }
+def fix_n_bit_hex(num, n):
+    return '0' * (n - len(num)) + hex(num)[2:]
 
-    return reg_dict[reg]
+def translate_push(arg):
+    if str.isdigit(arg):
+        val = int(arg)
+        if val <= 0xfffff:
+            instruction = append_opcode(OPCODES.PUSHI.value) + '0'
+            return append_immediate(instruction, val) 
+        else:
+            instruction = append_opcode(OPCODES.PUSHL.value) + '0'
+            return append_large_val(instruction, val) 
+    elif is_reg(arg):
+        instruction = append_opcode(OPCODES.PUSHR.value)
+        return append_first_reg_operand(arg) + (5*'0') + '\n'
 
-def translate_instruction(instruction):
-    return
+def translate_rirlrr(args, instr_dict):
+    reg = args[0]
+    arg2 = args[1]
+    if str.isdigit(arg2):
+        val = int(arg2)
+        if val <= 0xfffff:
+            instruction = append_opcode(instr_dict['ri'])
+            instruction = append_first_reg_operand(instruction, reg)
+            return append_immediate(instruction, val) 
+        else:
+            instruction = append_opcode(instr_dict['rl'])
+            instruction = append_first_reg_operand(instruction, reg)
+            return append_large_val(instruction, val) 
+    elif is_reg(arg2):
+        instruction = append_opcode(instr_dict['rr'])
+        instruction = append_first_reg_operand(instruction, reg)
+        return append_second_reg_operand(arg2)
+
+def translate_jump(opcode, arg):
+    instruction = append_opcode(opcode)
+    instruction = append_address(instruction, arg)
+    return instruction
+
+def append_opcode(opcode):
+    return fix_n_bit_hex(opcode, 2)
+
+def append_immediate(instruction, val):
+    return instruction + fix_n_bit_hex(int(val), 5) + '\n'
+
+def append_large_val(instruction, val):
+    return instruction + (5*'0') + '\n' + fix_n_bit_hex(int(val), 8) + '\n'
+
+#we append two zeroes in the middle since only jump instructions append addresses
+def append_address(instruction, addr):
+    return instruction + ('0' * 2) + fix_n_bit_hex(int(addr), 4) + '\n'
+
+def append_first_reg_operand(instruction, reg):
+    return instruction + reg_dict[reg]
+
+def append_second_reg_operand(instruction, reg):
+    return instruction + ('0' * 4) + reg_dict[reg] + '\n'
+
+def translate_instruction(line):
+    tokens = line.split()
+    mnemonic = tokens[0]
+    args = tokens[1:]
+    if mnemonic == "END":
+        return fix_n_bit_hex(OPCODES.END.value, 8) + '\n'
+    if mnemonic == "NOP":
+        return fix_n_bit_hex(OPCODES.NOP.value, 8) + '\n'
+    if mnemonic == "PUSH":
+        if arg_rules["PUSH"]:
+            return translate_push(line)
+    if mnemonic == "POP":
+        if arg_rules["POP"]:
+            instruction = append_opcode(OPCODES.POP.value)
+            instruction = append_first_reg_operand(instruction, args[0])
+            return instruction + 5 * '0' + '\n'
+    if mnemonic == "LDR":
+        if arg_rules["LDR"]:
+            instruction = append_opcode(OPCODES.LDR.value)
+            instruction = append_first_reg_operand(instruction, args[0])
+            instruction = append_second_reg_operand(instruction, args[1])
+            return instruction
+    if mnemonic == "STR":
+        if arg_rules["STR"]:
+            instruction = append_opcode(OPCODES.STR.value)
+            instruction = append_first_reg_operand(instruction, args[0])
+            instruction = append_second_reg_operand(instruction, args[1])
+            return instruction
+    if mnemonic == "MOV":
+        if arg_rules["MOV"]:
+            return translate_rirlrr(args, mov_dict)
+    if mnemonic == "ADD":
+        if arg_rules["ADD"]:
+            return translate_rirlrr(args, add_dict)
+    if mnemonic == "SUB":
+        if arg_rules["SUB"]:
+            return translate_rirlrr(args, sub_dict)
+    if mnemonic == "MUL":
+        if arg_rules["MUL"]:
+            return translate_rirlrr(args, mul_dict)
+    if mnemonic == "IMUL":
+        if arg_rules["IMUL"]:
+            return translate_rirlrr(args, imul_dict)
+    if mnemonic == "DIV":
+        if arg_rules["DIV"]:
+            return translate_rirlrr(args, div_dict)
+    if mnemonic == "IDIV":
+        if arg_rules["IDIV"]:
+            return translate_rirlrr(args, idiv_dict)
+    if mnemonic == "CMP":
+        if arg_rules["CMP"]:
+            return translate_rirlrr(args, cmp_dict)
+    if mnemonic == "JMP":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JMP.value, args[0])
+    if mnemonic == "JNZ":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JNZ.value, args[0])
+    if mnemonic == "JZ":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JZ.value, args[0])
+    if mnemonic == "JG":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JG.value, args[0])
+    if mnemonic == "JGE":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JGE.value, args[0])
+    if mnemonic == "JL":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JL.value, args[0])
+    if mnemonic == "JLE":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JLE.value, args[0])
+    if mnemonic == "JA":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JA.value, args[0])
+    if mnemonic == "JAE":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JAE.value, args[0])
+    if mnemonic == "JB":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JB.value, args[0])
+    if mnemonic == "JBE":
+        if arg_rules["JMP"]:
+            return translate_jump(OPCODES.JBE.value, args[0])
+    if mnemonic == "NOP":
+        if arg_rules["NOP"]:
+            instruction = append_opcode(OPCODES.NOP.value)
+            instruction = append_first_reg_operand(instruction, args[0])
+            return instruction + 5 * '0' + '\n'
+    if mnemonic == "AND":
+        if arg_rules["AND"]:
+            return translate_rirlrr(args, and_dict)
+    if mnemonic == "OR":
+        if arg_rules["OR"]:
+            return translate_rirlrr(args, or_dict)
+    if mnemonic == "XOR":
+        if arg_rules["XOR"]:
+            return translate_rirlrr(args, xor_dict)
+
+
+
+
 
 def main():
-    print(translate_register("R15"))
+    print(reg_dict["R15"])
     return
 
 
---
+++ translations.py
@@ -0,0 +1,172 @@
+from enum import auto, Enum
+
+def is_reg(arg):
+    return arg in reg_dict.keys()
+
+def validate_address(arg):
+    return str.isdigit(arg) and int(arg) <= 0xffff
+
+def validate_num_val(arg):
+    return str.isdigit(arg) & int(arg) <= 0xffffffff
+
+def rirlrr_validation(args):
+    return len(args) == 2 and is_reg(args[0]) and (validate_num_val(args[1]) or is_reg(args[1]))
+
+class OPCODES(Enum):
+    END = 0 
+    NOP = auto()
+    PUSHI = auto()
+    PUSHL = auto()
+    PUSHR = auto()
+    POP = auto()
+    LDR = auto()
+    STR = auto()
+    MOVRI = auto() 
+    MOVRL = auto() 
+    MOVRR = auto()
+    ADDRI = auto() 
+    ADDRL = auto() 
+    ADDRR = auto()
+    SUBRI = auto() 
+    SUBRL = auto() 
+    SUBRR = auto()
+    MULRI = auto() 
+    MULRL = auto() 
+    MULRR = auto()
+    DIVRI = auto() 
+    DIVRL = auto() 
+    DIVRR = auto()
+    IMULRI  = auto()
+    IMULRL  = auto()
+    IMULRR = auto()
+    IDIVRI  = auto()
+    IDIVRL  = auto()
+    IDIVRR  = auto()
+    CMPRI  = auto()
+    CMPRL   = auto()
+    CMPRR = auto()
+    JMP  = auto()
+    JNZ  = auto()
+    JZ  = auto()
+    JG  = auto()
+    JGE  = auto()
+    JL  = auto()
+    JLE  = auto()
+    JA  = auto()
+    JAE  = auto()
+    JB  = auto()
+    JBE = auto()
+    NOT = auto()
+    ANDRI  = auto()
+    ANDRL  = auto()
+    ANDRR = auto()
+    ORRI  = auto()
+    ORRL  = auto()
+    ORRR = auto()
+    XORRI  = auto()
+    XORRL = auto()
+    XORRR = auto()
+
+label_table = {}
+
+reg_dict = {
+    "R0": "0", 
+    "R1": "1", 
+    "R2": "2", 
+    "R3": "3", 
+    "R4": "4", 
+    "R5": "5", 
+    "R6": "6", 
+    "R7": "7",
+    "R8": "8",
+    "R9": "9",
+    "R10": "a",
+    "R11": "b",
+    "R12": "c",
+    "R13": "d",
+    "R14": "e",
+    "R15": "f",
+    "FL": "c",
+    "PC": "d",
+    "BP": "e",
+    "SP": "f"
+    }
+
+mov_dict ={
+    "ri": OPCODES.MOVRI.value,
+    "rl": OPCODES.MOVRL.value,
+    "rr": OPCODES.MOVRR.value
+}
+add_dict ={
+    "ri": OPCODES.ADDRI.value,
+    "rl": OPCODES.ADDRL.value,
+    "rr": OPCODES.ADDRR.value
+}
+sub_dict ={
+    "ri": OPCODES.SUBRI.value,
+    "rl": OPCODES.SUBRL.value,
+    "rr": OPCODES.SUBRR.value
+}
+mul_dict ={
+    "ri": OPCODES.MULRI.value,
+    "rl": OPCODES.MULRL.value,
+    "rr": OPCODES.MULRR.value
+}
+imul_dict ={
+    "ri": OPCODES.IMULRI.value,
+    "rl": OPCODES.IMULRL.value,
+    "rr": OPCODES.IMULRR.value
+}
+div_dict ={
+    "ri": OPCODES.DIVRI.value,
+    "rl": OPCODES.DIVRL.value,
+    "rr": OPCODES.DIVRR.value
+}
+idiv_dict ={
+    "ri": OPCODES.IDIVRI.value,
+    "rl": OPCODES.IDIVRL.value,
+    "rr": OPCODES.IDIVRR.value
+}
+cmp_dict ={
+    "ri": OPCODES.CMPRI.value,
+    "rl": OPCODES.CMPRL.value,
+    "rr": OPCODES.CMPRR.value
+}
+and_dict ={
+    "ri": OPCODES.ANDRI.value,
+    "rl": OPCODES.ANDRL.value,
+    "rr": OPCODES.ANDRR.value
+}
+or_dict ={
+    "ri": OPCODES.ORRI.value,
+    "rl": OPCODES.ORRL.value,
+    "rr": OPCODES.ORRR.value
+}
+xor_dict ={
+    "ri": OPCODES.XORRI.value,
+    "rl": OPCODES.XORRL.value,
+    "rr": OPCODES.XORRR.value
+}
+
+arg_rules = {
+    "PUSH": lambda args: len(args) == 1 and (validate_num_val(args[0]) or is_reg(args[0])),
+    "POP":  lambda args: len(args) == 1 and is_reg(args[0]),
+    "LDR":  lambda args: len(args) == 2 and is_reg(args[0]) and is_reg(args[1]),
+    "STR":  lambda args: len(args) == 2 and is_reg(args[0]) and is_reg(args[1]),
+    "MOV":  rirlrr_validation,
+    "ADD":  rirlrr_validation,
+    "SUB":  rirlrr_validation,
+    "MUL":  rirlrr_validation,
+    "IMUL": rirlrr_validation,
+    "DIV":  rirlrr_validation,
+    "IDIV": rirlrr_validation,
+    "CMP":  rirlrr_validation,
+    "JMP":  lambda args: len(args) == 1 and validate_address(args[0]),
+    "NOT":  lambda args: len(args) == 1 and is_reg(args[0]),
+    "AND":  rirlrr_validation,
+    "OR":  rirlrr_validation,
+    "XOR":  rirlrr_validation,
+}
+
+data_seg_start = 0x6000
+code_seg_start = 0x8000
